f2069292d050126cd890ca1b325891dd0d382b14
nicholaspohlmann <pohlmann.nick@gmail.com>
2021-06-06 08:31:53 -0400

    Finished the project, added in the output string, and computation for the total savings

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,10 +16,27 @@ public class App {
         String rateString = myApp.readInput("What is the rate? ");
         String yearsString = myApp.readInput("What is the number of years? ");
         String compoundsPerYearString = myApp.readInput("What is the number of times the interest is compounded per year? ");
-        float principal = myApp.convertInputToFloat(principalString);
-        float rate = myApp.convertInputToFloat(rateString);
+        double principal = myApp.convertInputToDouble(principalString);
+        double interestRatePercentage = myApp.convertInputToDouble(rateString);
         int years = myApp.convertInputToInt(yearsString);
-        float compoundsPerYear = myApp.convertInputToFloat(compoundsPerYearString);
+        double compoundsPerYear = myApp.convertInputToDouble(compoundsPerYearString);
+        double interestRateDecimal = myApp.computeInterestRateDecimal(interestRatePercentage);
+        double totalSavings = myApp.computeTotalSavings(principal,years,interestRateDecimal,compoundsPerYear);
+        String outputSring = myApp.generateOutputSring(principal,interestRatePercentage, years, totalSavings, compoundsPerYear);
+        myApp.printOutputString(outputSring);
+    }
+
+    private void printOutputString(String outputSring) {
+        System.out.println(outputSring);
+    }
+
+    private String generateOutputSring(double principal, double interestRatePercentage, int years, double totalSavings, double compoundsPerYear) {
+        return "$" + principal + " invested at " + interestRatePercentage +"% for " + years + "years compounded " + compoundsPerYear + " times per year is $" + totalSavings;}
+
+    private double computeTotalSavings(double principal, int years, double interestRateDecimal, double compoundsPerYear) {
+        double totalSavings = principal * Math.pow((1 + (interestRateDecimal / compoundsPerYear)),(compoundsPerYear*years));
+        totalSavings = (Math.floor(totalSavings*100)/100);
+        return totalSavings;
     }
 
     private String readInput(String str) {
@@ -33,8 +50,13 @@ public class App {
         return input;
     }
 
-    private float convertInputToFloat(String inputString) {
-        float input = Float.parseFloat(inputString);
+    private double convertInputToDouble(String inputString) {
+        double input = Double.parseDouble(inputString);
         return input;
+    }
+
+    private double computeInterestRateDecimal(double interestRatePercentage) {
+        double interestRateDecimal =  interestRatePercentage / 100;
+        return interestRateDecimal;
     }
 }
